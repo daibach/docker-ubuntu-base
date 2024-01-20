cac11237dda69aa8b3e872c2d91e1575c2caae48
Dafydd Vaughan <dafydd.vaughan@cedyrn.com>
2024-01-20 17:32:05 +0000

    add initial generation script

---
+++ generate.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -xe
+
+gen() {
+  VERSION=$1
+  NAME=$2
+
+  mkdir -p ${NAME}
+  echo "FROM ubuntu:${VERSION}" > ${NAME}/Dockerfile
+  echo '' >> ${NAME}/Dockerfile
+
+  echo '# Install dependencies' >> ${NAME}/Dockerfile
+  echo 'RUN apt-get -qq update && apt-get -qq install -y --no-install-recommends git zlib1g-dev ca-certificates libpng-dev libzip-dev libicu-dev zip unzip default-mysql-client cron' >> ${NAME}/Dockerfile
+
+  echo '' >> ${NAME}/Dockerfile
+
+  echo '# Set up cron' >> ${NAME}/Dockerfile
+  echo 'RUN touch /var/log/cron.log' >> ${NAME}/Dockerfile
+  echo '' >> ${NAME}/Dockerfile
+
+  echo '# Install AWS CLI' >> ${NAME}/Dockerfile
+  echo 'RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"' >> ${NAME}/Dockerfile
+  echo 'RUN unzip awscliv2.zip' >> ${NAME}/Dockerfile
+  echo 'RUN ./aws/install' >> ${NAME}/Dockerfile
+  echo '' >> ${NAME}/Dockerfile
+
+  echo '# Install Hugo' >> ${NAME}/Dockerfile
+  echo 'RUN apt-get -qq install -y --no-install-recommends hugo' >> ${NAME}/Dockerfile
+  echo '' >> ${NAME}/Dockerfile
+
+  echo '' >> ${NAME}/Dockerfile
+}
